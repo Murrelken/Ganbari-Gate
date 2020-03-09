808fad5c4df616075bb02f12e1555512c0741ebb
Murrelken <DMButin@stud.kpfu.ru>
2020-03-09 22:11:21 +0300

    master init

---
+++ GanbariGate/GanbariGate/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using GanbariGate.Models;
+
+namespace GanbariGate.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
